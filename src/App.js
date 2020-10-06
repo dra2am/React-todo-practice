72933f381270cfaabf5d04776a155bda63fc46c1
Diana A <dralcantara96@gmail.com>
2020-10-06 19:02:54 -0400

    Fix delete function

--- src/App.js
+++ src/App.js
@@ -2,43 +2,40 @@ import React from "react";
 import AddTask from "./components/AddTask";
 import TaskList from "./components/TaskList";
 import { without } from "lodash";
+
 class App extends React.Component {
   state = {
-    list: []
+    list: [],
   };
 
   //temp hold state of current list as var
   //take tempState recieved from child and add to list array
   //set this as new state
-  addTodo = newTodo => {
+  addTodo = (newTodo) => {
     let tempList = this.state.list;
     tempList.unshift(newTodo);
     this.setState({
-      list: tempList
+      list: tempList,
     });
   };
 
-  //ISSUES WITH DELETING
+  //Deleting with lodash's without
   //take item from tasklist.js
-  //get rid of it with lodash
-  deleteItem = item => {
+  deleteItem = (item) => {
     let tempList = this.state.list;
     //without: lodash method, takes array in first arg, returns array without what you specified in second arg
     let removeItem = without(tempList, item);
     this.setState({
-      list: removeItem
+      list: removeItem,
     });
   };
 
-  //contains the state of list
-  //passes it to child
-  todoList = this.state.list;
-
   render() {
+    //passing functions and state to components as prop
     return (
       <div>
         <AddTask addTodo={this.addTodo} />
-        <TaskList todoList={this.todoList} deleteItem={this.deleteItem} />
+        <TaskList todoList={this.state.list} deleteItem={this.deleteItem} />
       </div>
     );
   }
